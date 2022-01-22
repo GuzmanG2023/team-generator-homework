449330a118c2c39ddc7d672c6fd51dc13874ccf5
St4d3ntG4zm4n <bwonder91@live.com>
2022-01-21 19:05:30 -0700

    final updates

--- src/html.js
+++ src/html.js
@@ -1,3 +1,34 @@
+const divGenerator = (data) => {
+    let result = ''
+    for(let i = 0; i < data.length; i++) {
+        if (data[i].officeNumber) {
+            result += `<div class=\"manager\">
+            <h2>Manager</h2>
+            <p>${data[i].name}</p>
+                <p>${data[i].id}</p>
+                <a href="mailto:${data[i].email}">${data[i].email}</a>
+                <p>${data[i].officeNumber}</p></div>`
+        }
+        else if (data[i].github) {
+            result += `<div class=\"engineer\">
+            <h2>Engineer</h2>
+            <p>${data[i].name}</p>
+                    <p>${data[i].id}</p>
+                    <a href="mailto:${data[i].email}">${data[i].email}</a>
+                    <a href="https://github.com/${data[i].github}">https://github.com/${data[i].github}</a></div>`
+        }
+        else {
+            result += `<div class=\"intern\">
+            <h2>Intern</h2>
+            <p>${data[i].name}</p>
+                    <p>${data[i].id}</p>
+                    <a href="mailto:${data[i].email}">${data[i].email}</a>
+                    <p>${data[i].school}</p></div>`
+        }
+    }
+    return result
+}
+
 const html = (data) => {
     return `<!DOCTYPE html>
     <html lang="en">
@@ -5,9 +36,15 @@ const html = (data) => {
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>My Team</title>
+        <title>Team Generator</title>
         <link rel="stylesheet" href="style.css">
-    </head>`
+    </head>
+    <body>
+        <header>
+            <h1>Generator Crew</h1>
+        </header>
+    ${divGenerator(data)}
+    </body>`
 }
 
 module.exports = {html}
